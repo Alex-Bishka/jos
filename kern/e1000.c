fef4b5a996c1486f06cfe959c5d1eeb155bf8226
Alex Bishka <abishka@cs.hmc.edu>
2020-11-16 16:25:14 -0800

    just finished exercise 4

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,4 +1,16 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
+#include <inc/string.h>
+
+static volatile char* e1000_base;
+#define E1000_ADDR(offset) (*(volatile uint32_t*) (e1000_base + offset))
 
 // LAB 6: Your driver code here
+int
+e1000_attach(struct pci_func *pcif)
+{
+	pci_func_enable(pcif);
+	e1000_base = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+	cprintf("Device status register value is: %x\n", E1000_ADDR(E1000_STATUS));
+	return 1;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,3 +1,10 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
+
+#include <kern/pci.h>
+
+int e1000_attach(struct pci_func *pcif);
+
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+
 #endif  // SOL >= 6
