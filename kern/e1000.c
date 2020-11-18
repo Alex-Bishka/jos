44d0aaaf9fac6185ae9290dce38d37a110760fd1
Alex Nghiem <anghiem@cs.hmc.edu>
2020-11-17 20:30:56 -0800

    finished exercise 5

--- kern/e1000.c
+++ kern/e1000.c
@@ -13,5 +13,13 @@ e1000_attach(struct pci_func *pcif)
 	e1000_base = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
 	cprintf("Device status register value is: %x\n", E1000_ADDR(E1000_STATUS));
 	struct PageInfo * pp = page_alloc(ALLOC_ZERO);
+	E1000_ADDR(E1000_TDBAL) = (uint32_t) page2kva(pp);
+	E1000_ADDR(E1000_TDLEN) = 1 << 10; // each queue is size 1k 
+	E1000_ADDR(E1000_TDH) = 0;
+	E1000_ADDR(E1000_TDT) = 0;
+	E1000_ADDR(E1000_TCTL) |= E1000_TCTL_EN | E1000_TCTL_PSP;
+	E1000_ADDR(E1000_TCTL) |= 0x10 << 4; // this is to set Collision Threshold to desired value of 10h
+	E1000_ADDR(E1000_TCTL) |= 0x40 << 12; // this is to set Collision Distance to expected value of 40h
+
 	return 1;
-}
+} 
--- kern/e1000.h
+++ kern/e1000.h
@@ -6,5 +6,13 @@
 int e1000_attach(struct pci_func *pcif);
 
 #define E1000_STATUS   0x00008  /* Device Status - RO */
+#define E1000_TCTL 	0x00400	/* TX Control - RW */
+#define E1000_TDBAL 	0x03800 /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH 	0x03804 /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
 
 #endif  // SOL >= 6
