1cb7245dead8e9d4b4bf20f4933d7ff7fe570b1d
Alex Bishka <abishka@cs.hmc.edu>
2020-11-24 11:51:20 -0800

    just finished exercise 10

--- kern/e1000.c
+++ kern/e1000.c
@@ -4,18 +4,21 @@
 
 static volatile char* e1000_base;
 struct tx_desc* tx_desc_arr;
+struct rx_desc* rx_desc_arr;
 char* send_bufptr[64];
+char* recv_bufptr[64];
 #define E1000_ADDR(offset) (*(volatile uint32_t*) (e1000_base + offset))
-#define NUM_DESC 64
+#define NUM_TX_DESC 64
+#define NUM_RX_DESC 64
+#define MAC_ADDR_LOW 0x12005452
+#define MAC_ADDR_HIGH 0x80005634
 
 
 // todo:
-// 1) change va's to pa's (where e1000 card needs an addr)
-// 2) set all fields tx_desc
-// 	a) cmd's bits as well
-// 3) after 1 and 2 run a dump
-// Do we need to set cso/cmd/css/special?
-// I think cso and css? is 0b
+// 1) RAL/RAH?
+// 2) What should RDT/RDH be set to? 
+// 3) RDMTS?
+// 4) MO?
 
 int
 e1000_attach(struct pci_func *pcif)
@@ -34,7 +37,7 @@ e1000_attach(struct pci_func *pcif)
 	E1000_ADDR(E1000_TCTL) |= 0x40 << 12; // this is to set Collision Distance to expected value of 40h
 	E1000_ADDR(E1000_TIPG) |= (0x6 << 20) | (0x8 << 10) | 0xA; 
 	
-	for (int i = 0; i < NUM_DESC; i += 2) {
+	for (int i = 0; i < NUM_TX_DESC; i += 2) {
 		struct PageInfo* pg = page_alloc(ALLOC_ZERO);
 		void* pgaddr = page2kva(pg);
 		send_bufptr[i] = pgaddr;
@@ -48,6 +51,29 @@ e1000_attach(struct pci_func *pcif)
 		tx_desc_arr[i+1].cso = 0; 
 		tx_desc_arr[i+1].status |= 1; // DD bit
 	}
+	struct PageInfo* pp_rx = page_alloc(ALLOC_ZERO);
+	rx_desc_arr = (struct rx_desc*) page2kva(pp_rx);
+	E1000_ADDR(E1000_MTA) = 0;
+	// E1000_ADDR(E1000_IMS) |= E1000_IMS_LSC | E1000_IMS_RXSEQ | E1000_IMS_RXDMT0 | E1000_IMS_RXO | E1000_IMS_RXT0; 
+	E1000_ADDR(E1000_RAL) = MAC_ADDR_LOW;
+	E1000_ADDR(E1000_RAH) = MAC_ADDR_HIGH;
+	E1000_ADDR(E1000_RDBAL) = page2pa(pp_rx);
+	E1000_ADDR(E1000_RDLEN) = 1 << 11; // each queue is size 1k 
+	E1000_ADDR(E1000_RDH) = 1; // double check this
+	E1000_ADDR(E1000_RDT) = 0;
+	E1000_ADDR(E1000_RCTL) |= E1000_RCTL_BAM | E1000_RCTL_SECRC;
+	
+	for (int i = 0; i < NUM_RX_DESC; i += 2) {
+		struct PageInfo* pg = page_alloc(ALLOC_ZERO);
+		void* pgaddr = page2kva(pg);
+		recv_bufptr[i] = pgaddr;
+		recv_bufptr[i+1] = pgaddr + PGSIZE/2;
+		rx_desc_arr[i].addr = (uint64_t) ((uint32_t) PADDR(recv_bufptr[i]));
+		rx_desc_arr[i].status |= (1 << 1); // EOP bit
+		rx_desc_arr[i+1].addr = (uint64_t) ((uint32_t) PADDR(recv_bufptr[i+1]));
+		rx_desc_arr[i+1].status |= (1 << 1); // EOP bit
+	}
+	E1000_ADDR(E1000_RCTL) |= E1000_RCTL_EN;
 	return 1;
 } 
 
@@ -61,7 +87,7 @@ transmit_packet(char* buf, size_t size) {
 	memmove(send_bufptr[E1000_ADDR(E1000_TDT)], buf, size);
 	desc->length = size; // may need to check if size is at least 48 bytes	
 	desc->status &= ~1;
-	E1000_ADDR(E1000_TDT) = (E1000_ADDR(E1000_TDT) + 1) % NUM_DESC;
+	E1000_ADDR(E1000_TDT) = (E1000_ADDR(E1000_TDT) + 1) % NUM_TX_DESC;
 	return 0;
 }
 
--- kern/e1000.h
+++ kern/e1000.h
@@ -8,6 +8,7 @@ int transmit_packet(char* buf, size_t size);
 
 #define MAX_PACKET_SIZE	0x1518	/* Max size of TX packet */
 
+// for transmit
 #define E1000_STATUS   0x00008  /* Device Status - RO */
 #define E1000_TCTL 	0x00400	/* TX Control - RW */
 #define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
@@ -19,7 +20,30 @@ int transmit_packet(char* buf, size_t size);
 #define E1000_TCTL_EN     0x00000002    /* enable tx */
 #define E1000_TCTL_PSP    0x00000008    /* pad short packets */
 
+// for recv
+#define E1000_MTA      0x05200  /* Multicast Table Array - RW Array */
+#define E1000_IMS      0x000D0  /* Interrupt Mask Set - RW */
+#define E1000_ICR_LSC           0x00000004 /* Link Status Change */
+#define E1000_ICR_RXSEQ         0x00000008 /* rx sequence error */
+#define E1000_ICR_RXDMT0        0x00000010 /* rx desc min. threshold (0) */
+#define E1000_ICR_RXO           0x00000040 /* rx overrun */
+#define E1000_ICR_RXT0          0x00000080 /* rx timer intr (ring 0) */
+#define E1000_RAL       0x05400  /* Receive Address Low - RW Array */
+#define E1000_RAH       0x05404  /* Receive Address High - RW Array */
+#define E1000_RDBAL    0x02800  /* RX Descriptor Base Address Low - RW */
+#define E1000_RDBAH    0x02804  /* RX Descriptor Base Address High - RW */
+#define E1000_RDLEN    0x02808  /* RX Descriptor Length - RW */
+#define E1000_RDH      0x02810  /* RX Descriptor Head - RW */
+#define E1000_RDT      0x02818  /* RX Descriptor Tail - RW */
+#define E1000_RCTL     0x00100  /* RX Control - RW */
+#define E1000_RCTL_EN             0x00000002    /* enable */
+
 #define E1000_TXD_STAT_DD    0x00000001 /* Descriptor Done */
+#define E1000_RCTL_LPE            0x00000020    /* long packet enable */
+#define E1000_RCTL_LBM_MAC        0x00000040    /* MAC loopback mode */
+#define E1000_RCTL_BAM            0x00008000    /* broadcast enable */
+#define E1000_RCTL_SZ_2048        0x00000000    /* rx buffer size 2048 */
+#define E1000_RCTL_SECRC          0x04000000    /* Strip Ethernet CRC */
 
 
 struct tx_desc
@@ -30,6 +54,16 @@ struct tx_desc
 	uint8_t cmd;
 	uint8_t status;
 	uint8_t css;
+	uint16_t special;
+};
+
+struct rx_desc
+{
+	uint64_t addr;
+	uint16_t length;
+	uint16_t psc;
+	uint8_t status;
+	uint8_t errors;
 	uint16_t special;
 };
 
