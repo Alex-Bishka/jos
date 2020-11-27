e273ee611f7329fc37af53f23e79ddee5c9f87e6
Alex Nghiem <anghiem@cs.hmc.edu>
2020-11-27 14:11:27 -0800

    stuck on single-stepping

--- user/breakpoint.c
+++ user/breakpoint.c
@@ -5,6 +5,10 @@
 void
 umain(int argc, char **argv)
 {
+	cprintf("Starting function\n");
 	asm volatile("int $3");
+	cprintf("between breakpoints\n");
+	asm volatile("int $3");
+	cprintf("After second breakpoint\n");
 }
 
